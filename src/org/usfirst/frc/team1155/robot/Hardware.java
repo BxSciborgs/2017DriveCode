a81df4a96db6ec3447bd8fe657deecc0d01d986e
HugGa <galloh@bxscience.edu>
2016-11-30 19:05:18 -0500

    Added gitignore + hardware file

---
+++ src/org/usfirst/frc/team1155/robot/Hardware.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1155.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
+
+public enum Hardware {
+	INSTANCE;
+	public CANTalon frontLeftT, frontRightT, backLeftT, backRightT;
+	public Joystick rightJ, leftJ, gamePad;
+	Hardware()
+	{
+		frontLeftT = new CANTalon(1);
+		frontRightT = new CANTalon(2);
+		backLeftT = new CANTalon(3);
+		backRightT = new CANTalon(4);
+		leftJ = new Joystick(0);
+		rightJ = new Joystick(1);
+		gamePad = new Joystick(2);
+	}
+}

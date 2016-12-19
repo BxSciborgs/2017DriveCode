a87689756209a589721b277d4aebf4be4cfd6c81
Karun Ellango <ellangok@bxscience.edu>
2016-12-19 16:38:32 -0500

    Update PortMap.java

--- src/org/usfirst/frc/team1155/robot/PortMap.java
+++ src/org/usfirst/frc/team1155/robot/PortMap.java
@@ -5,25 +5,33 @@ import edu.wpi.first.wpilibj.CANTalon;
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
+ * the wiring easier and significantly reduces the number of magic static final numbers
  * floating around.
  */
-public enum PortMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
-	
-	PORT;
-	
-	public int leftJoy = 0;
-	public int rightJoy = 1;
-	
-	public int frontLeftTalonDM = 0, frontRightTalonDM = 1, backLeftTalonDM = 2, backRightTalonDM = 3, frontLeftTalonSM = 4, frontRightTalonSM = 5, backLeftTalonSM = 6, backRightTalonSM = 7;
+public static final enum PortMap {
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static final static static final int leftMotor = 1;
+	// public static final static static final int rightMotor = 2;
 
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static final static static final int rangefinderPort = 1;
+	// public static final static static final int rangefinderModule = 1;
+
+	PORT;
+
+	public static final int leftJoy = 0;
+	public static final int rightJoy = 1;
+
+	public static final int FLdrive = 1;
+	public static final int FLsteer = 2;
+	public static final int RLdrive = 3;
+	public static final int RLsteer = 4;
+	public static final int RRdrive = 5;
+	public static final int RRsteer = 6;
+	public static final int FRdrive = 7;
+	public static final int FRsteer = 8;
+	
+	public static final int gyro = 0;
 }

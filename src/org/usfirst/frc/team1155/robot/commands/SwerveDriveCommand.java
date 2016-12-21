c65020b99c36ca780723d291a4b6ee0bdf5baedb
Karun Ellango <ellangok@bxscience.edu>
2016-12-21 16:39:22 -0500

    Copy pasta execute from old version

--- src/org/usfirst/frc/team1155/robot/commands/SwerveDriveCommand.java
+++ src/org/usfirst/frc/team1155/robot/commands/SwerveDriveCommand.java
@@ -51,7 +51,63 @@ public class SwerveDriveCommand extends Command{
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
+		forwardRaw = (leftJoy.getY() + rightJoy.getY())/2;
+     	strafeRaw = (leftJoy.getX() + rightJoy.getX())/2;
+     	rotateClockwiseRaw = leftJoy.getZ();
+     	
+     	temp = (forwardRaw * Math.cos(gyro.getAngle())) + strafeRaw * Math.sin(gyro.getAngle());
+     	strafeFieldCentric = (-forwardRaw * Math.sin(gyro.getAngle())) + strafeRaw * Math.cos(gyro.getAngle());
+     	forwardFieldCentric = temp;
+     	
+    	wheelbase = 30; //length of drivebase
+   	trackwidth = 24; //width of drivebase
+     	diagonal = Math.sqrt((wheelbase*wheelbase) + (trackwidth*trackwidth));
+     	
+     	a = strafeFieldCentric - rotateClockwiseRaw * (wheelbase/diagonal);
+     	b = strafeFieldCentric + rotateClockwiseRaw * (wheelbase/diagonal);
+     	c = forwardFieldCentric - rotateClockwiseRaw * (trackwidth/diagonal);
+     	d = forwardFieldCentric + rotateClockwiseRaw * (trackwidth/diagonal);
+     	
+     	frontRSpeed = Math.sqrt(b*b + c*c);
+     	frontLSpeed = Math.sqrt(b*b + d*d);
+     	backLSpeed = Math.sqrt(a*a + d*d);
+     	backRSpeed = Math.sqrt(a*a + c*c);
+     	frontRAngle = Math.atan2(b,c) * 180/Math.PI;
+     	frontLAngle = Math.atan2(b,d) * 180/Math.PI;
+     	backRAngle = Math.atan2(a,d) * 180/Math.PI;
+     	backLAngle = Math.atan2(a,c) * 180/Math.PI;
+     	
+     	//Because the CANTalon position control uses values from 0 - 1023 
+     	//for potentiometer ranges, we must modify the wheel angles to 
+     	//compenstate for this. To do this add 180 to the wheel angle to 
+     	//get a 0-360 range then multiply by 1023/360 which equals 2.8444...
+     	Robot.drive.setAngle((frontRAngle+180) * (1023/360),
+     				   (frontLAngle+180) * (1023/360),
+     				   (backLAngle+180) * (1023/360), 
+     				   (backRAngle+180) * (1023/360));
+     	
+     	//Normalize wheel speeds as stated in Ether's document
+     	max = frontRSpeed;
+     	if(frontLSpeed>max){
+     		max = frontLSpeed;
+     	}
+     	if(backLSpeed>max){
+     		max = backLSpeed;
+     	}
+     	if(backRSpeed>max){
+     		max = backRSpeed;
+     	}
+     	if(max>1){
+     		frontRSpeed/=max;
+     		frontLSpeed/=max;
+     		backRSpeed/=max;
+     		backLSpeed/=max;
+     	}
+     	
+     	Robot.drive.setSpeed(frontRSpeed, frontLSpeed, backRSpeed, backLSpeed);
+  				  		
+  	}		  	}
+  		  
 		
 	}
 
